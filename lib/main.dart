10ea7787f31f9616e27ff92094577da11807431b
snozeestars <snozerhma@gmail.com>
2025-12-14 12:25:19 +0700

    Take Services tampilan coba #47

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
 import 'features/worker/presentation/pages/worker_page.dart';
+import 'features/worker/domain/entities/worker.dart';
+import 'features/worker/domain/repositories/worker_repository.dart';
+import 'features/worker/domain/usecase/accjob_worker.dart';
+
+class _FakeWorkerRepository implements WorkerRepository {
+  final Worker sampleWorker = Worker(
+    idWorker: 1,
+    idAccount: 1,
+    role: 'Plumber',
+    statusAvailable: true,
+  );
+
+  @override
+  Future<Worker> getWorkerById(int idWorker) async => sampleWorker;
+
+  @override
+  Future<List<Worker>> getAvailableWorkers() async => [sampleWorker];
+
+  @override
+  Future<void> updateWorkerStatus({required int idWorker, required bool statusAvailable}) async {
+    // simulate network/db delay
+    await Future.delayed(const Duration(milliseconds: 200));
+  }
+}
 
 void main() {
-  runApp(const MyApp());
+  final repo = _FakeWorkerRepository();
+  final acc = AccJobWorker(repo);
+  final worker = repo.sampleWorker;
+
+  runApp(MyApp(worker: worker, accJobWorker: acc));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Worker worker;
+  final AccJobWorker accJobWorker;
+
+  const MyApp({super.key, required this.worker, required this.accJobWorker});
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +48,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: const Color(0xfff4f6f8),
       ),
-      home: const WorkerPage(),
+      home: WorkerPage(worker: worker, accJobWorker: accJobWorker),
     );
   }
 }
