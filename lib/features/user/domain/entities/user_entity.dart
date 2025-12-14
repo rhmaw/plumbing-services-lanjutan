42a96213cfa01ea6d7d778de0d251bc661068f09
RatnaA178 <praptiwiratna04@gmail.com>
2025-12-14 15:11:55 +0700

    profile user

---
+++ lib/features/user/domain/entities/user_entity.dart
@@ -0,0 +1,17 @@
+class UserEntity {
+  final String id;
+  final String name;
+  final String email;
+  final String phone;
+  final String address;
+  final String avatarUrl;
+
+  UserEntity({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.phone,
+    required this.address,
+    required this.avatarUrl,
+  });
+}
--- lib/features/user/domain/repositories/user_repository.dart
+++ lib/features/user/domain/repositories/user_repository.dart
@@ -1,8 +1,9 @@
-
 import 'package:plumbing_services_pml_kel4/features/user/domain/entities/booking_entity.dart';
 
 abstract class UserRepository {
-Future<List<BookingServiceEntity>> getOrderHistory(String status);
+  Future<List<BookingServiceEntity>> getOrderHistory(String status);
 
-Future<void> createBooking(BookingServiceEntity booking);
+  Future<void> createBooking(BookingServiceEntity booking);
+
+  getUserProfile() {}
 }
