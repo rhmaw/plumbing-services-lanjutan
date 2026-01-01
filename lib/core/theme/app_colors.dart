2904455c0df275ae50f9feb12750f1100e1e0e51
Rahma Wati <snozerhma@gmail.com>
2026-01-01 19:31:52 +0700

    Theme File

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBlue = Color(0xFF6395F8); 
+  static const Color iconBlue = Color(0xFF6395F8);
+  
+  static const Color textGrey = Color(0xFF888888);
+  static const Color borderGrey = Color(0xFFE0E0E0);
+  
+  static final Color blobColor = Colors.blue.withOpacity(0.3);
+}
