e25937568af251d9993fce95c9371d4b4f0e25ea
snozeestars <snozerhma@gmail.com>
2026-01-04 13:24:24 +0700

    edit colors page

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 class AppColors {
@@ -9,16 +8,4 @@ class AppColors {
   static const Color borderGrey = Color(0xFFE0E0E0);
   
   static final Color blobColor = Colors.blue.withOpacity(0.3);
-=======
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const Color primaryBlue = Color(0xFF6395F8); 
-  static const Color iconBlue = Color(0xFF6395F8);
-  
-  static const Color textGrey = Color(0xFF888888);
-  static const Color borderGrey = Color(0xFFE0E0E0);
-  
-  static final Color blobColor = Colors.blue.withOpacity(0.3);
->>>>>>> 2904455c0df275ae50f9feb12750f1100e1e0e51
 }
